2a6e1edc2d72eb9b5a9f479b008008585eff169c
An4oys1012 <ankos1990@gmail.com>
2023-03-31 20:29:06 +0300

    add models

--- film/film/settings.py
+++ film/film/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'film',
-        'USER': 'anna',
+        'USER': 'postgres',
         'PASSWORD': '8168163',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
